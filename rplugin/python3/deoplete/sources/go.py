39321cf4775b90bd422a6eefd753489fe3bffd1a
Koichi Shiraishi <zchee.io@gmail.com>
2015-12-03 23:06:24 +0900

    Support gocode and vim-go
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- rplugin/python3/deoplete/sources/go.py
+++ rplugin/python3/deoplete/sources/go.py
@@ -9,12 +9,21 @@ class Source(Base):
         self.name = 'go'
         self.mark = '[go]'
         self.filetypes = ['go']
-        self.rank = 1000
+        self.rank = 100
         self.min_pattern_length = 0
         self.is_bytepos = True
 
-    def get_complete_position(self, context):
-        return self.vim.call('go#complete#Complete', 1, 0)
+    def get_complete_api(self, findstart):
+        self.complete_api = self.vim.vars['deoplete#sources#go']
+        if self.complete_api == 'gocode':
+            return self.vim.call('gocomplete#Complete', findstart, 0)
+        elif self.complete_api == 'vim-go':
+            return self.vim.call('go#complete#Complete', findstart, 0)
+        else:
+            return deoplete.util.error(self.vim, "g:deoplete#sources#go is 'gocode' or 'vim-go'")
 
-    def gather_candidates(self, context, **kwargs):
-        return self.vim.call('go#complete#Complete', 0, 0)
+    def get_complete_position(self, context):
+        return self.get_complete_api(1)
+
+    def gather_candidates(self, context):
+        return self.get_complete_api(0)
